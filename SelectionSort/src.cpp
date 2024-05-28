ebe1ee8910d85dd3875b2464cf55cd5f07872dae
Prab <prab.missier@gmail.com>
2024-05-28 20:20:36 +0930

    Selection Sort

---
+++ SelectionSort/src.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+class SelSort
+{	
+public:
+	vector<int> doSort(vector<int> inp)
+	{		
+		vector<int> op;
+		while(inp.size()>0)
+		{
+			int min = inp[0];
+			int minidx = 0;
+			for (int j = 0; j < inp.size(); ++j)
+			{
+				if (inp[j] < min) {
+					min = inp[j];
+					minidx = j;
+				}
+			}
+
+			op.push_back(min);
+			inp.erase(inp.begin() + minidx);
+		}
+		return op;		
+	}
+	friend ostream& operator<<(ostream& o, vector<int> v);
+};
+
+ostream& operator<<(ostream& op, vector<int>& vin)
+{
+	for (int i = 0; i < vin.size(); ++i)
+		op << vin[i] << " ";
+	op << "\n";
+
+	return op;
+
+}
+
+int main()
+{
+
+	SelSort ssrt;
+	vector<int> vinp = { 99,12,1,55,23,19 };
+
+	vector<int> out = ssrt.doSort(vinp);
+	cout << out;
+
+	return 0;
+}
